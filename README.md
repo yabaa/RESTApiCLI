2eb906acae5b91cf3a43bf971679d38d3aa0929b
Yahya Baassou <yahya.baassou@zenika.com>
2019-02-27 17:02:27 +0800

    [IMPROVE] update loopback version to 4.X

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # How to build a RESTful API from CLI
 
-This tuto will show you how to build a RESTful API from CLI in 5 minutes. The database used in the back-end will be MongoDB. (You can also use another database)
+This tuto will show you how to build a RESTful API from CLI in 5 minutes.
+The database used in the back-end will be MongoDB. (You can also use another database)
 
 ### Pre-requisite:
 
-* MongoDB server running locally
-* Node.js installed
+* [MongoDB server](https://docs.mongodb.com/manual/administration/install-community/) running locally
+* [Node.js](https://nodejs.org/en/download/) installed
 
-### Step 1: Install Loopback
-What's [Loopback](https://loopback.io/doc/index.html) ?
-> npm install -g loopback-cli
+### Step 1: Install Loopback 4.x
+What's [Loopback](https://loopback.io/doc/en/lb4/Getting-started.html) ?
+> npm i -g @loopback/cli
 
 ### Step 2: Create application
 Make a directory where you wish to store your project. Open the folder you have created and run the following commands :
 
 * Create application
 Make sure you are in the right folder and type :
-> lb
+> lb app
 * Run application
 > node .
 
@@ -27,16 +28,24 @@ The application can be accessed from http://localhost:3000.
 * Install MongoDB Connector
 > npm install --save loopback-connector-mongodb
 * Connect MongoDB Connector
-> lb datasource mongoDS --connector mongoDB
+> lb4 datasource mongoDS --connector mongoDB
 
 ### Step 4: Creating Data models
 Run following command to create data models:
-> lb model
+> lb4 model
 
 Note that if you need to have relations between each models, you can use :
-> lb relation
+> lb4 relation >>>not working
 
-### Step 5: Authentication
+### Step 5: Creating Data repositories
+Run following command to create data repositories:
+> lb4 repository
+
+### Step 6: Creating Data controllers
+Run following command to create data controllers:
+> lb4 controller
+
+### Step 7: Authentication
 > lb acl
 
 You need to create a user and call login api which return you an access_token.
--- presentation/spectacle-boilerplate/presentation/index.js
+++ presentation/spectacle-boilerplate/presentation/index.js
@@ -12,6 +12,7 @@ import {
   BlockQuote,
   ComponentPlayground,
   Cite,
+  Code,
   Heading,
   ListItem,
   List,
@@ -56,13 +57,15 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
         <Slide transition={["spin"]} bgImage={images.soccerBackground} bgDarken={0.7} textColor="tertiary">
-        <Appear>
-          <Heading size={3} textColor="primary" caps>The project</Heading>
+          <Appear>
+            <Heading size={3} textColor="primary" caps>The project</Heading>
           </Appear>
           <Appear>
-          <Text margin="10px 0 0" textColor="primary" size={3}>
-            RESTful API which manage professional soccer players and teams with <br/>a back-end on Node.js <br />and a MongoDB
-          </Text>
+            <Text margin="10px 0 0" textColor="primary" size={3}>
+              RESTful API which manage professional soccer players and teams with <br/>
+              a back-end on Node.js <br />
+              and a MongoDB
+            </Text>
           </Appear>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
@@ -72,6 +75,16 @@ export default class Presentation extends React.Component {
             <ListItem>Creating application</ListItem>
             <ListItem>Creating data models</ListItem>
           </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps>What Loopback is?</Heading>
+          <Image src="https://media.giphy.com/media/YalzdbnnMFTBm/giphy.gif" margin="0px center 100px" height="500"/>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps>Loopback</Heading>
+          <Text margin="10px 0 0" textColor="secondary" size={3}>
+          The LoopBack framework is a set of Node.js modules that you can use independently or together to quickly build REST APIs.
+          </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={3} textColor="secondary" caps>Pre-requisite</Heading>
@@ -84,20 +97,22 @@ export default class Presentation extends React.Component {
           <Heading size={3} fit textColor="secondary">
             Install LoopBack
           </Heading>
-            npm install -g loopback-cli
+            npm i -g @loopback/cli
         </Slide>
         <Slide transition={['spin']}>
           <Heading size={3} fit textColor="secondary">
             Creating Application
           </Heading>
           <Appear>
-            <Heading size={6} textColor="tertiary">command : lb</Heading>
+            <Code>lb4 app</Code>
           </Appear>
           <Appear>
             <List>
-              <ListItem>Application name</ListItem>
-              <ListItem>LoopBack version</ListItem>
-              <ListItem>Application type</ListItem>
+              <ListItem>Project name</ListItem>
+              <ListItem>Project description</ListItem>
+              <ListItem>Project root directory</ListItem>
+              <ListItem>Application class name</ListItem>
+              <ListItem>Select features to enable</ListItem>
             </List>
           </Appear>
         </Slide>
@@ -106,16 +121,15 @@ export default class Presentation extends React.Component {
             Installing MongoDB Connector
           </Heading>
           <Appear>
-            <Heading textColor="tertiary" fit>command : npm install --save loopback-connector-mongodb</Heading>
+            <Code>npm install --save loopback-connector-mongodb</Code>
           </Appear>
-
         </Slide>
         <Slide transition={['spin']}>
           <Heading size={3} fit textColor="secondary">
             Connecting MongoDB Connector
           </Heading>
           <Appear>
-            <Heading textColor="tertiary" fit>command : lb datasource mongoDS --connector mongoDB</Heading>
+            <Code>lb4 datasource mongoDS --connector mongoDB</Code>
           </Appear>
           <Appear>
             <List>
@@ -130,7 +144,7 @@ export default class Presentation extends React.Component {
             Creating model
           </Heading>
           <Appear>
-            <Heading textColor="tertiary" size={6}>command : lb model</Heading>
+            <Code>lb4 model</Code>
           </Appear>
           <Appear>
             <List>
@@ -143,10 +157,55 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide transition={['spin']}>
           <Heading size={3} fit textColor="secondary">
+            Creating repositories
+          </Heading>
+          <Appear>
+            <Code>lb4 repository</Code>
+          </Appear>
+          <Appear>
+            <List>
+              <ListItem>Datasource</ListItem>
+              <ListItem>Select Model to generate Repository (press 'a' for all)</ListItem>
+            </List>
+          </Appear>
+        </Slide>
+        <Slide transition={['spin']}>
+          <Heading size={3} fit textColor="secondary">
+            Creating controllers
+          </Heading>
+          <Appear>
+            <Code>lb4 controller</Code>
+          </Appear>
+          <Appear>
+            <List>
+              <ListItem>Controller class name</ListItem>
+              <ListItem>Kind of controller (CRUD)</ListItem>
+              <ListItem>Name of the model</ListItem>
+              <ListItem>Name of the repository</ListItem>
+              <ListItem>Type of id defined in the model</ListItem>
+              <ListItem>Base of HTTP path name</ListItem>
+            </List>
+          </Appear>
+        </Slide>
+        <Slide transition={['spin']}>
+          <Heading size={3} fit textColor="secondary">
+            Generated folder hierarchy
+          </Heading>
+          <Appear>
+            <List>
+              <ListItem>Datasources: ./src/datasources</ListItem>
+              <ListItem>Controllers: ./src/controllers</ListItem>
+              <ListItem>Models: ./src/models</ListItem>
+              <ListItem>Repositories: ./src/repositories</ListItem>
+            </List>
+          </Appear>
+        </Slide>
+        <Slide transition={['spin']}>
+          <Heading size={3} fit textColor="secondary">
             Creating relation
           </Heading>
           <Appear>
-            <Heading textColor="tertiary" size={6} >command : lb relation</Heading>
+            <Code>lb relation</Code>
           </Appear>
         </Slide>
         <Slide transition={["fade"]} bgImage={images.homerPlay} bgDarken={0.8} textColor="primary">
@@ -156,7 +215,7 @@ export default class Presentation extends React.Component {
         <Slide transition={["fade"]} bgColor="primary">
           <Heading size={3} textColor="secondary" caps>What about authentication?</Heading>
           <Appear>
-            <Heading textColor="tertiary" size={6} >command : lb acl</Heading>
+            <Code>lb acl</Code>
           </Appear>
           <Appear>
             <List>
